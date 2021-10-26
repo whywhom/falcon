525f96fb45b6cc8fc4ec87fd13305679a326e136
awu <awu@mega.co.nz>
2021-10-27 12:37:40 +1300

    update

--- controllers/defaultConfig.js
+++ controllers/defaultConfig.js
@@ -4,7 +4,7 @@ const config = {
     database: {
         DATABASE: 'falconDb', //数据库名称
         USERNAME: 'root', //mysql用户名falconAdmin
-        PASSWORD: 'qazwsxedc123', //mysql密码qazwsxedc123
+        PASSWORD: 'qazwsxedc123', //mysql密码qaz123
         PORT: '3306', //mysql端口号
         HOST: 'localhost' //服务器ip
     }
